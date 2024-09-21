bbc2cb06dffec1135589760a8387b10dac42f4e1
Yure <91153560+YureNathan@users.noreply.github.com>
2024-09-21 04:02:31 -0300

    Finalizando logica

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let qtdItens = 0;
-
+let nome = " ";
+let precoPrato = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
 function verificarPedido() {
   const botaoPedido = document.querySelector(".textoPedido");
   if (qtdItens === 3) {
@@ -21,24 +24,46 @@ function selecionarItens(elemento, categoria) {
 }
 function selecionarItemPf(elemento) {
   selecionarItens(elemento, "pf");
+  let precoElement = elemento.querySelector(".preco");
+  let nameElement = elemento.querySelector(".titulo");
+  //   nome = nameElement.textContent;
+  precoPrato = precoElement.textContent;
 }
 function selecionarItemBebida(elemento) {
   selecionarItens(elemento, "bebida");
+  let precoElement = elemento.querySelector(".preco");
+  let nameElement = elemento.querySelector(".titulo");
+  //   nome = nameElement.textContent;
+  precoBebida = precoElement.textContent;
 }
 
 function selecionarItemSobremesa(elemento) {
   selecionarItens(elemento, "sobremesa");
+  let precoElement = elemento.querySelector(".preco");
+  let nameElement = elemento.querySelector(".titulo");
+  //   nome = nameElement.textContent;
+  precoSobremesa = precoElement.textContent;
 }
-function finlizarPedido(elemento){
+function finlizarPedido() {
+  const modal = document.querySelector(".telaConfirmacao");
   // const acessando a classe do elemento e removendo ela ao clicar no botão
-  elemento.classList.remove("escondido")
-
-
+  modal.classList.remove("escondido");
 }
 
-// verificar como posso está removendo a classe do modal. 
+function voltarTela() {
+  const retornar = document.querySelector(".telaConfirmacao");
+  retornar.classList.add("escondido");
+}
+// verificar como posso está removendo a classe do modal.
 
-// fazer a função para pegar os valores e calcular 
-// e fazer exibir nome do prato e valor ao lado 
-// valor total 
+function valorTotal() {
+  const valor =
+    parseFloat(precoPrato) +
+    parseFloat(precoBebida) +
+    parseFloat(precoSobremesa);
+  return valor.toFixed(2);
+}
+// fazer a função para pegar os valores e calcular
+// e fazer exibir nome do prato e valor ao lado
+// valor total
 // depois fazer a formatação da mensagem para o whatsApp
